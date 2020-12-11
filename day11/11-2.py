d50784fe7aebea98e014b344748b4068fb078e1f
Liam Håkansson <liam.hakansson@afconsult.com>
2020-12-11 10:07:46 +0100

    Solved day 11 task 2

--- day11/11-2.py
+++ day11/11-2.py
@@ -9,45 +9,65 @@ def load_data():
     return rows
 
 
-def perform_iteration(seats):
+def check_diagonals(i, j, seats):
+    directions = ["r", "l", "u", "d", "ul", "ur", "dl", "dr"]
     nbr_rows = len(seats)
     nbr_cols = len(seats[0])
+    nbr_occupied_seen = 0
+    for dir in directions:
+        if dir == "r":
+            x, y = 0, 1
+        elif dir == "l":
+            x, y = 0, -1
+        elif dir == "u":
+            x, y = -1, 0
+        elif dir == "d":
+            x, y = 1, 0
+        elif dir == "ur":
+            x, y = -1, 1
+        elif dir == "ul":
+            x, y = -1, -1
+        elif dir == "dr":
+            x, y = 1, 1
+        elif dir == "dl":
+            x, y = 1, -1
 
+        for distance in range(1, max(nbr_rows, nbr_cols)):
+            x_dist = x * distance
+            y_dist = y * distance
+            if i + x_dist < 0 or i + x_dist >= nbr_rows or j + y_dist < 0 or j + y_dist >= nbr_cols:
+                break
+            if seats[i + x_dist][j + y_dist] == "#":
+                nbr_occupied_seen += 1
+                break
+            if seats[i + x_dist][j + y_dist] == "L":
+                break
+    return nbr_occupied_seen
+
+
+def perform_iteration(seats):
     new_seats = []
 
     for i, row in enumerate(seats):
         new_row = []
         for j, seat in enumerate(row):
-            nbr_occupied_adj = 0
 
             if seats[i][j] == ".":
                 new_row.append(".")
 
-            for x in range(max(0, i-1), min(i+1, nbr_rows-1)+1):
-                for y in range(max(0, j-1), min(j+1, nbr_cols-1)+1):
-                    if x != i or y != j:
-                        if seats[x][y] == "#":
-                            nbr_occupied_adj += 1
+            nbr_occupied_seen = check_diagonals(i, j, seats)
             if seats[i][j] == "L":
-                if nbr_occupied_adj == 0:
+                if nbr_occupied_seen == 0:
                     new_row.append("#")
                 else:
                     new_row.append("L")
             elif seats[i][j] == "#":
-                if nbr_occupied_adj >= 4:
+                if nbr_occupied_seen >= 5:
                     new_row.append("L")
                 else:
                     new_row.append("#")
         new_seats.append(new_row)
     return new_seats
-
-
-def flatten(list):
-    flat_list = []
-    for sublist in list:
-        for item in sublist:
-            flat_list.append(item)
-    return flat_list
 
 
 def count_occupied(seats):
