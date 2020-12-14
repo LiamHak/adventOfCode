1bf657179245645fcb0a39a360ed680d78086db8
Liam Håkansson <liam.hakansson@afconsult.com>
2020-12-14 16:56:40 +0100

    Solved task 2

--- day14/14-2.py
+++ day14/14-2.py
@@ -11,10 +11,29 @@ def load_data():
 
 
 def apply_mask(num, mask):
-    bit_num = bin(num)[2:].zfill(36)
-    new_bit_num = [bit if mask[i] == "X" else mask[i] for i, bit in enumerate(bit_num)]
-    bit_num = "".join(new_bit_num)
-    return bit_num
+    masks = all_masks(mask)
+    results = []
+    for old_mask in masks:
+        bit_num = bin(num)[2:].zfill(36)
+        new_bit_num = [bit if old_mask[i] == "X" else old_mask[i] for i, bit in enumerate(bit_num)]
+        bit_num = "".join(new_bit_num)
+        results.append(bit_num)
+
+    return results
+
+
+def all_masks(mask):
+    if not mask:
+        yield ''
+        return
+    for m in all_masks(mask[1:]):
+        if mask[0] == '0':
+            yield 'X' + m  # leave unchanged
+        elif mask[0] == '1':
+            yield '1' + m  # replace with 1
+        elif mask[0] == 'X':
+            yield '0' + m  # replace with 0
+            yield '1' + m  # replace with 1
 
 
 if __name__ == '__main__':
@@ -26,9 +45,10 @@ if __name__ == '__main__':
         if inst[0] == "mask":
             mask = inst[1]
         else:
-            bit_num = apply_mask(int(inst[1]), mask)
-            mem_address = int(inst[0].strip("mem[").strip("]"))
-            mem[mem_address] = int(bit_num, 2)
+            bit_nums = int(inst[1])
+            mem_addresses = apply_mask(int(inst[0].strip("mem[").strip("]")), mask)
+            for mem_address in mem_addresses:
+                mem[mem_address] = bit_nums
 
     total_memory_values = sum(mem.values())
 
